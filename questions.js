7889e7c7b002a20c55e15f5688889e854bc0227d
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-07-10 12:52:50 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"My family and I … in London when I was young.",
+        options:["lived", "was live", "live"],
+        answer: 0
+    },
+    {
+        q:"We … some sandwiches and fresh fruit to eat for lunch yesterday.",
+        options:["buy", "bought", "buys"],
+        answer: 1
+    },
+    {
+        q:"They wanted to … a movie but there were no more tickets.",
+        options:["seeing", "saw", "see"],
+        answer: 2
+    },
+    {
+        q:"Did you have a good time? - Yes, I ….  .",
+        options:["did", "have", "had"],
+        answer: 0
+    },
+    {
+        q:"He didn’t … me because I was behind the tree.",
+        options:["saw", "see", "sees"],
+        answer: 1
+    },
+    {
+        q:" … you a good student? - Yes, I was.",
+        options:["Did", "Was", "Were"],
+        answer: 2
+    },
+    {
+        q:"When did they … back to their country?",
+        options:["fly", "flew", "flies"],
+        answer: 0
+    },
+    {
+        q:"They … back to their country last month.",
+        options:["flies", "flew", "fly"],
+        answer: 1
+    },
+    {
+        q:"Did you … lots of interesting photos on your holiday?",
+        options:["took", "takes", "take"],
+        answer: 2
+    },
+    {
+        q:"We had a great time and we … lots of fun and exciting things.",
+        options:["did", "was did", "do"],
+        answer: 0
+    },
+    {
+        q:"Why … you finish your math homework last week?",
+        options:["wasn't", "didn't", "weren't"],
+        answer: 1
+    },
+    {
+        q:"He … see a dentist yesterday because he had a toothache.",
+        options:["have to", "has to", "had to"],
+        answer: 2
+    },
+    {
+        q:"I wanted to … , but I couldn’t. I had to stay and help my friend.",
+        options:["go", "went", "going"],
+        answer: 0
+    },
+    {
+        q:"She didn’t answer the phone because she … at home.",
+        options:["weren't", "wasn't", "didn't"],
+        answer: 1
+    },
+    {
+        q:" … they tired after the long trip? - Yes, they ….  .",
+        options:["Were / was", "Was / were", "Were / were"],
+        answer: 2
+    }
+
+
+
+]
